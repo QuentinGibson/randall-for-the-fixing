6c509a82af41a3938bbd0e41f567f62c701a0fc8
Quentin Gibson <quentingibson94@gmail.com>
2023-04-25 20:19:47 +0000

    add admin tests

---
+++ cypress/e2e/admin.cy.ts
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker";
+describe("Admin page", () => {
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("should create new content", () => {
+    
+  })
+
+  it("should see contact requests", () => {
+
+  })
+
+  it("should be able to delete content", () => {
+
+  })
+
+  it("should have billing", () => {
+
+  })
+
+  it("should be able to update content", () => {
+
+  })
+});
+
+describe("One Post", () => {
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("should have a image", () => {
+
+  })
+
+  it("should have content", () => {
+
+  })
+  it("should have recent posts", () => {
+
+  })
+  it("should have a next/previous button", () => {
+
+  })
+})
