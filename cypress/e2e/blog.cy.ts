c0af249948d31f043db57ec8ece23d66d5806467
Quentin Gibson <quentingibson94@gmail.com>
2023-04-25 20:05:31 +0000

    single blog post test

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -23,16 +23,17 @@ describe("One Post", () => {
   });
 
   it("should have a image", () => {
-
+    cy.get("tp-blog-details-thumb img").should('have.attr', 'src')
   })
 
   it("should have content", () => {
-
+    cy.get("tp-blog-details-content").should('have.length.gte', 1)
   })
   it("should have recent posts", () => {
-
+    cy.get("tp-blog-sidebar__post").should('have.length.gte', 1)
   })
   it("should have a next/previous button", () => {
-
+    cy.get("postbox__more-left postbox-more-icon a").should('have.attr', 'href').and('include', 'blog')
+    cy.get("postbox__more-right postbox-more-icon a").should('have.attr', 'href').and('include', 'blog')
   })
 })
