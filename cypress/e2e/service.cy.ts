f74d8f51175095e8d36575cf0d1189a227a359b4
Quentin Gibson <quentingibson94@gmail.com>
2023-04-25 20:19:27 +0000

    service page testing

---
+++ cypress/e2e/service.cy.ts
@@ -0,0 +1,37 @@
+describe("All Services Page", () => {
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("shows services", () => {
+    cy.get(".tp-service-2-item").should('have.length.gte', 1)
+  })
+
+  it("should contain testimonies", () => {
+    cy.get(".tp-testimonial-2-wrapper h3").should('have.length.gte', 1)
+  })
+
+});
+
+describe("One Service", () => {
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("should be able to search for more services", () => {
+
+  })
+
+  it("should have a image", () => {
+    cy.get(".tp-service-details-thumb img").should('have.attr', 'src')
+  })
+  it("should have all its content", () => {
+    cy.get(".tp-service-details-text-title").first().should('have.length.gte', 1)
+  })
+  it("should have other services", () => {
+    cy.get(".#service-sidebar .service-sidebar__widget-content ul").should('have.length.gte', 1)
+  })
+  it("should have a gallery", () => {
+    cy.get("tp-service-details-thumb-2 img").should('have.length.gte', 1)
+  })
+})
